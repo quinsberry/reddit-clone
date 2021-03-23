52d491c211f244ec247b481a077eb9a86d4d94d6
Yevhenii <eugeniusz.kolesnyk@gmail.com>
2021-03-23 09:13:37 +0200

    - client: fixed bug when unauthorized user could create a post

--- client/src/pages/r/[sub]/submit.tsx
+++ client/src/pages/r/[sub]/submit.tsx
@@ -5,6 +5,7 @@ import { Post, Sub } from '@tps/data.types'
 import Head from 'next/head'
 import { FormEvent, useState } from 'react'
 import axios from 'axios'
+import { GetServerSideProps } from 'next'
 
 
 export default function SubmitPage() {
@@ -85,4 +86,17 @@ export default function SubmitPage() {
             {sub && <Sidebar sub={sub} />}
         </div>
     )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+    try {
+        const cookie = req.headers.cookie
+        if (!cookie) throw new Error('Missing auth token cookie')
+
+        await axios.get('/auth/me', { headers: { cookie } })
+
+        return { props: {} }
+    } catch (err) {
+        res.writeHead(307, { location: '/login' }).end()
+    }
 }
